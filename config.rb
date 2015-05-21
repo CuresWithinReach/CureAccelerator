b356a3f5664bdf8093f3bd1fdf19b65e728e08b3
kurtcunningham <cunningham.kurt@gmail.com>
2015-05-21 14:18:27 -0500

    still working

--- config.rb
+++ config.rb
@@ -23,9 +23,9 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
-  activate :asset_hash
+  # activate :asset_hash
   activate :directory_indexes
   activate :gzip
-  activate :imageoptim
+  activate :imageoptim, pngout: false
   activate :relative_assets
 end
