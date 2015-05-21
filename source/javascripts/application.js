6be6947966acd3457b39424b7672df61d1c751b1
kurtcunningham <cunningham.kurt@gmail.com>
2015-05-21 11:45:36 -0500

    finished homepage

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,14 @@
 //= require 'jquery/dist/jquery.js'
 //= require 'picturefill/dist/picturefill.js'
 //= require_tree .
+
+
+$(function () {
+  var $card_trigger = $('.card--trigger');
+  $card_trigger.on('click', function (e) {
+    var is_active = $('.card--trigger.is-active');
+    e.preventDefault();
+    is_active.removeClass('is-active');
+    $(this).addClass('is-active');
+  });
+});
