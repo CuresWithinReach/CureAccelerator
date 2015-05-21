1cddd5d53e190af98a2482d3e3edf16d3b7d5ebf
kurtcunningham <cunningham.kurt@gmail.com>
2015-05-21 14:12:21 -0500

    one more try

--- config.rb
+++ config.rb
@@ -16,16 +16,16 @@ end
 set :css_dir,    'stylesheets'
 set :js_dir,     'javascripts'
 set :images_dir, 'images'
+set :relative_links, true
 
 # Build-specific configuration
 configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
-  # activate :asset_hash
-  # activate :directory_indexes
+  activate :asset_hash
+  activate :directory_indexes
   activate :gzip
   activate :imageoptim
   activate :relative_assets
-  set :relative_links, true
 end
