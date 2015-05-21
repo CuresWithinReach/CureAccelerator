4ea0e0678e9a1854ea51c4562db985b7358f3990
kurtcunningham <cunningham.kurt@gmail.com>
2015-05-21 14:55:15 -0500

    removed picture tag. boo.

--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
-  # activate :asset_hash
+  activate :asset_hash
   activate :directory_indexes
   activate :gzip
   activate :imageoptim, pngout: false
